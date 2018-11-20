7a5bf9b8bd50d6a85a5bfe3c2b42cd32af7fa82d
Giulia <giuliaravasi@MBP-di-Giulia-2.lan>
2018-11-20 23:40:42 +0100

    code

--- sketch.js
+++ sketch.js
@@ -1,11 +1,48 @@
-function preload(){
-  // put preload code here
+var value = 0;
+
+var myImage1;
+var myImage2;
+var mainText = 'Hey, this is Charlie!'
+var mainText2 = 'Turn your device around to discover his main moods!'
+
+function preload() {
+  myImage1 = loadImage("./assets/play.svg");
+  myImage2 = loadImage("./assets/sleep.svg");
+  myImage3 = loadImage("./assets/eat.svg");
+  myImage4 = loadImage("./assets/sad.svg");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
+  textFont('Roboto');
+  imageMode(CENTER);
+
+  setShakeThreshold(35);
+
+ background("#00e6ac");
+
+ image(myImage1, width/2, height/2, myImage1.width/2, myImage1.height/2);
+
 }
 
 function draw() {
-  // put drawing code here
+fill(255);
+textAlign(CENTER);
+textSize(30);
+text(mainText, width/2, height/6);
+text(mainText2, width/2, height/6 + 40);
+}
+
+function deviceTurned() {
+  if(turnAxis === 'X') {
+    background('#4d79ff');
+    image(myImage2, width/2, height/2 + 100, myImage2.width/2, myImage2.height/2);
+  } else if(turnAxis === 'Z') {
+    background('#ff9933');
+    image(myImage3, width/2, height/2 + 100, myImage2.width/2, myImage2.height/2);
+  } else if(turnAxis === 'Y') {
+    background('#8585ad');
+    image(myImage4, width/2, height/2 + 100, myImage2.width/2, myImage2.height/2);
+  }
 }
